ea06c2557c85bcbbed906df49c36834aa692d314
Anay dongre <anaydongre@BBMDVANAYD.local>
2021-10-04 17:53:55 +0530

    basics done

---
+++ README.md
@@ -0,0 +1,9 @@
+Notes
+
+Interpolation
+{{ }} syntax in vue is called interpolation
+It only works between opeaning and closing HTML tags, not in tag properties
+
+Vue Directives
+v-bind
+v-html
--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoal: "Finish the course and learn vue",
-        }
-    }
-})
+  data() {
+    return {
+      courseGoal1: "Finish the course and learn vue",
+      courseGoal2: "Finish the course and Master vue",
+      vueLink: "https://vuejs.org",
+      htmlContent: "<li>I am HTML</li>",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      return randomNumber > 0.5 ? this.courseGoal1 : this.courseGoal2;
+    },
+  },
+});
 
-app.maount("#user-goal")
+app.mount("#user-goal");
